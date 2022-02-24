6aab5b1ddc0acad35d034c49d7cb8ecb8d465793
huy <huy@gamejam.com>
2022-02-24 12:07:03 +0700

    feat: Vesting contract

---
+++ contracts/Vesting.sol
@@ -0,0 +1,202 @@
+/* SPDX-License-Identifier: UNLICENSED */
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Vesting is Ownable {
+    struct Program {
+        uint256 id;
+        string name;
+        uint256 start;
+        uint256 end;
+        uint256 availableAmount;
+        uint256 tgeUnlockPercentage;
+        uint256 unlockMoment;
+        uint256 blockUnlockPercentage;
+    }
+
+    struct VestingInfo {
+        uint256 claimedAmount;
+        mapping(uint256 => uint256) atProgram;
+    }
+
+    uint256 public TGE;
+    Program[] public allPrograms;
+    uint256 private _block;
+    mapping(address => bool) private _operators;
+    mapping(address => VestingInfo) private _vestingInfoOf;
+
+    event ProgramCreated(
+        uint256 id,
+        string name,
+        uint256 start,
+        uint256 end,
+        uint256 initialAmount,
+        uint256 tgeUnlockPercentage,
+        uint256 unlockMoment,
+        uint256 blockUnlockPercentage
+    );
+    event ParticipantRegistered(
+        address participant,
+        uint256 programId,
+        uint256 amount
+    );
+    event TGEStarted();
+    event ClaimSuccessful(address participant, uint256 amount);
+    event EmergencyWithdrawn(address recipient, uint256 amount);
+
+    constructor(uint256 block_) Ownable() {
+        _block = block_;
+        _operators[msg.sender] = true;
+    }
+
+    modifier onlyOperator() {
+        require(_operators[msg.sender], "Caller is not operator");
+        _;
+    }
+
+    function allProgramsLength() external view returns (uint256) {
+        return allPrograms.length;
+    }
+
+    function getVestingAmount(address participant, uint256 programId)
+        external
+        view
+        returns (uint256)
+    {
+        return _vestingInfoOf[participant].atProgram[programId];
+    }
+
+    function getClaimedAmount(address participants)
+        external
+        view
+        returns (uint256)
+    {
+        return _vestingInfoOf[participants].claimedAmount;
+    }
+
+    function getClaimableAmount(address participants)
+        public
+        view
+        returns (uint256)
+    {
+        uint256 totalUnlockedAmount = 0;
+        for (uint256 i = 0; i < allPrograms.length; i++) {
+            uint256 vestingAmount = _vestingInfoOf[participants].atProgram[i];
+            if (vestingAmount > 0) {
+                Program memory program = allPrograms[i];
+                if (block.timestamp >= TGE)
+                    totalUnlockedAmount =
+                        totalUnlockedAmount +
+                        (vestingAmount * program.tgeUnlockPercentage) /
+                        10000;
+                uint256 numUnlockTimes = (block.timestamp -
+                    program.unlockMoment) /
+                    _block +
+                    1;
+                totalUnlockedAmount =
+                    totalUnlockedAmount +
+                    (vestingAmount *
+                        program.blockUnlockPercentage *
+                        numUnlockTimes) /
+                    10000;
+            }
+        }
+        return totalUnlockedAmount - _vestingInfoOf[participants].claimedAmount;
+    }
+
+    function setOperators(address[] memory operators, bool[] memory isOperators)
+        external
+        onlyOwner
+    {
+        require(operators.length == isOperators.length, "Lengths mismatch");
+        for (uint256 i = 0; i < operators.length; i++)
+            _operators[operators[i]] = isOperators[i];
+    }
+
+    function createPrograms(
+        string[] memory names,
+        uint256[] memory starts,
+        uint256[] memory ends,
+        uint256[] memory initialAmounts,
+        uint256[] memory tgeUnlockPercentages,
+        uint256[] memory unlockMoments,
+        uint256[] memory blockUnlockPercentages
+    ) external onlyOperator {
+        require(names.length == starts.length, "Lengths mismatch");
+        require(names.length == ends.length, "Lengths mismatch");
+        require(names.length == initialAmounts.length, "Lengths mismatch");
+        require(
+            names.length == tgeUnlockPercentages.length,
+            "Lengths mismatch"
+        );
+        require(names.length == unlockMoments.length, "Lengths mismatch");
+        require(
+            names.length == blockUnlockPercentages.length,
+            "Lengths mismatch"
+        );
+        for (uint256 i = 0; i < names.length; i++) {
+            uint256 id = allPrograms.length;
+            allPrograms.push(
+                Program(
+                    id,
+                    names[i],
+                    starts[i],
+                    ends[i],
+                    initialAmounts[i],
+                    tgeUnlockPercentages[i],
+                    unlockMoments[i],
+                    blockUnlockPercentages[i]
+                )
+            );
+            emit ProgramCreated(
+                id,
+                names[i],
+                starts[i],
+                ends[i],
+                initialAmounts[i],
+                tgeUnlockPercentages[i],
+                unlockMoments[i],
+                blockUnlockPercentages[i]
+            );
+        }
+    }
+
+    function registerParticipant(address participant, uint256 programId)
+        external
+        payable
+        onlyOperator
+    {
+        require(participant != address(0), "Register the zero address");
+        require(programId < allPrograms.length, "Program does not exist");
+        Program storage program = allPrograms[programId];
+        require(block.timestamp >= program.start, "Program not available");
+        require(block.timestamp <= program.end, "Program is over");
+        require(msg.value <= program.availableAmount, "Not enough amount");
+        _vestingInfoOf[participant].atProgram[programId] += msg.value;
+        program.availableAmount -= msg.value;
+        emit ParticipantRegistered(participant, programId, msg.value);
+    }
+
+    function startTGE() external onlyOperator {
+        require(TGE == 0, "TGE already launched");
+        TGE = block.timestamp;
+        emit TGEStarted();
+    }
+
+    function claimTokens() external {
+        uint256 claimableAmount = getClaimableAmount(msg.sender);
+        _vestingInfoOf[msg.sender].claimedAmount += claimableAmount;
+        (bool success, ) = payable(msg.sender).call{value: claimableAmount}("");
+        require(success, "Claim tokens failed");
+        emit ClaimSuccessful(msg.sender, claimableAmount);
+    }
+
+    function emergencyWithdraw(address payable recipient) external onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = recipient.call{value: amount}("");
+        require(success, "Emergency withdraw failed");
+        emit EmergencyWithdrawn(recipient, amount);
+    }
+}
---
+++ test/test-vesting-program.js
@@ -0,0 +1,145 @@
+require('@nomiclabs/hardhat-ethers');
+
+const hre = require('hardhat');
+const { expect } = require("chai");
+const VESTING = "Vesting";
+
+before("Deploy Vesting contract", async () => {
+  // Prepare parameters
+  const [
+    deployer,
+    operator,
+    participant1,
+    participant2,
+    participant3,
+    participant4,
+    participant5,
+    participant6,
+    participant7,
+    participant8
+  ] = await hre.ethers.getSigners();
+  this.deployer = deployer;
+  this.operator = operator;
+  this.participants = [
+    participant1,
+    participant2,
+    participant3,
+    participant4,
+    participant5,
+    participant6,
+    participant7,
+    participant8
+  ];
+  this.purchaseAmounts = [
+    "20000000000000000000",
+    "30000000000000000000",
+    "40000000000000000000",
+    "50000000000000000000",
+    "35000000000000000000",
+    "50000000000000000000",
+    "17000000000000000000",
+    "102000000000000000000"
+  ];
+  this.block = 3;
+
+  // Deploy Vesting
+  this.vestingFactory = await hre.ethers.getContractFactory(VESTING);
+  this.vestingContract = await this.vestingFactory.deploy(this.block);
+  await this.vestingContract.deployed();
+});
+
+describe("Test Vesting contract", () => {
+  it("Set up Vesting contract", async () => {
+    await this.vestingFactory
+      .connect(this.deployer)
+      .attach(this.vestingContract.address)
+      .setOperators([this.operator.address], [true]);
+  });
+
+  it("Create 8 vesting programs", async () => {
+    let start = Math.floor(Date.now() / 1000) - 5;
+    let end = Math.floor(Date.now() / 1000) + 30;
+    let unlockMoment = Math.floor(Date.now() / 1000) + 3;
+    await this.vestingFactory
+      .connect(this.operator)
+      .attach(this.vestingContract.address)
+      .createPrograms(
+        [
+          "Seed Sale",
+          "Strategic Partners Sales",
+          "Community Growth",
+          "Ecosystem Growth",
+          "Team",
+          "Advisor",
+          "Treasury",
+          "IDO"
+        ],
+        [start, start, start, start, start, start, start, start],
+        [end, end, end, end, end, end, end, end],
+        [
+          "50000000000000000000000000",
+          "150000000000000000000000000",
+          "230000000000000000000000000",
+          "220000000000000000000000000",
+          "180000000000000000000000000",
+          "20000000000000000000000000",
+          "120000000000000000000000000",
+          "30000000000000000000000000"
+        ],
+        [0, 0, 3200, 2000, 0, 0, 1000, 10000],
+        [
+          unlockMoment,
+          unlockMoment,
+          unlockMoment,
+          unlockMoment,
+          unlockMoment,
+          unlockMoment,
+          unlockMoment,
+          unlockMoment
+        ],
+        [2000, 2000, 1360, 1600, 2000, 1250, 1125, 0]
+      );
+    let numPrograms = await this.vestingContract.allProgramsLength();
+    let treasuryInfo = await this.vestingContract.allPrograms(6);
+    expect(numPrograms.toString()).to.equal("8");
+    expect(treasuryInfo?.name).to.equal("Treasury");
+  });
+
+  it("Register 8 participants", async () => {
+    for (let i = 0; i < 7; i++)
+      await this.vestingFactory
+        .connect(this.operator)
+        .attach(this.vestingContract.address)
+        .registerParticipant(this.participants[i].address, i, { value: this.purchaseAmounts[i] });
+    let vestingAmounts = [];
+    for (let i = 0; i < 7; i++)
+      vestingAmounts.push(await this.vestingContract.getVestingAmount(this.participants[i].address, i));
+    for (let i = 0; i < 7; i++)
+      expect(vestingAmounts[i].toString()).to.equal(this.purchaseAmounts[i]);
+  });
+
+  it("Start TGE", async () => {
+    let TGE = await this.vestingContract.TGE();
+    expect(TGE.toString()).to.equal("0");
+    await this.vestingFactory
+      .connect(this.operator)
+      .attach(this.vestingContract.address)
+      .startTGE();
+    TGE = await this.vestingContract.TGE();
+    expect(TGE.toString()).to.not.equal("0");
+  });
+
+  it("Claim vesting tokens", async () => {
+    await sleep(12000);
+    await this.vestingFactory
+      .connect(this.participants[0])
+      .attach(this.vestingContract.address)
+      .claimTokens();
+    let claimedAmount = await this.vestingContract.getClaimedAmount(this.participants[0].address);
+    expect(claimedAmount.toString()).not.to.equal("0");
+  });
+});
+
+let sleep = ms => {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
