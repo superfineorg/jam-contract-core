f798075ed38474336a1b3fd6587f02d74398644b
huy <huy@gamejam.com>
2022-09-26 16:45:19 +0700

    feat: Playlink Airdrop smart contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,17 +5,23 @@ require("@nomiclabs/hardhat-etherscan");
 require('@openzeppelin/hardhat-upgrades');
 // require('@openzeppelin/hardhat-defender');
 require("@nomiclabs/hardhat-web3");
+require('hardhat-contract-sizer');
 
-const MNEMONIC = process.env.MNEMONIC;
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const BSC_API_KEY = process.env.BSC_API_KEY;
-const POLYGON_API_KEY = process.env.POLYGON_API_KEY;
-const ADDRESS_1 = process.env.ADDRESS_1;
+const ETHEREUM_PROVIDER = process.env.ETHEREUM_PROVIDER;
 const BSC_PROVIDER = process.env.BSC_PROVIDER;
+const JAMCHAIN_PROVIDER = process.env.JAMCHAIN_PROVIDER;
+const RINKEBY_PROVIDER = process.env.RINKEBY_PROVIDER;
+const GOERLI_PROVIDER = process.env.GOERLI_PROVIDER;
+const SEPOLIA_PROVIDER = process.env.SEPOLIA_PROVIDER;
 const BSC_TESTNET_PROVIDER = process.env.BSC_TESTNET_PROVIDER;
 const JAMCHAIN_TESTNET_PROVIDER = process.env.JAMCHAIN_TESTNET_PROVIDER;
 const POLYGON_TESTNET_PROVIDER = process.env.POLYGON_TESTNET_PROVIDER;
+const BSC_API_KEY = process.env.BSC_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const POLYGON_API_KEY = process.env.POLYGON_API_KEY;
+const ADDRESS = process.env.ADDRESS;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const MNEMONIC = process.env.MNEMONIC;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -29,64 +35,76 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {},
-    jamchaintestnet: {
-      url: JAMCHAIN_TESTNET_PROVIDER,
-      chainId: 2710,
+    eth: {
+      url: ETHEREUM_PROVIDER,
+      chainId: 1,
+      gas: 8000000,
+      accounts: { mnemonic: MNEMONIC }
+    },
+    bsc: {
+      url: BSC_PROVIDER,
+      chainId: 56,
+      gas: 8000000,
+      accounts: { mnemonic: MNEMONIC }
+    },
+    jamchain: {
+      url: JAMCHAIN_PROVIDER,
+      chainId: 2077,
       gas: 5500000,
-      accounts: { mnemonic: MNEMONIC },
-      from: ADDRESS_1
+      accounts: { mnemonic: MNEMONIC }
     },
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/" + INFURA_API_KEY,
+      url: RINKEBY_PROVIDER,
       chainId: 4,
       gas: 5500000,
-      accounts: { mnemonic: MNEMONIC },
-      from: ADDRESS_1
+      accounts: { mnemonic: MNEMONIC }
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/" + INFURA_API_KEY,
+      url: GOERLI_PROVIDER,
       chainId: 5,
       gas: 5500000,
-      accounts: { mnemonic: MNEMONIC },
-      from: ADDRESS_1
+      accounts: { mnemonic: MNEMONIC }
+    },
+    sepolia: {
+      url: SEPOLIA_PROVIDER,
+      chainId: 11155111,
+      gas: 5500000,
+      accounts: { mnemonic: MNEMONIC }
     },
     bsctestnet: {
       url: BSC_TESTNET_PROVIDER,
       chainId: 97,
       gas: 8000000,
       accounts: { mnemonic: MNEMONIC },
-      from: ADDRESS_1,
       networkCheckTimeout: 1000000000,
       timeoutBlocks: 200000000,
       skipDryRun: true,
+    },
+    jamchaintestnet: {
+      url: JAMCHAIN_TESTNET_PROVIDER,
+      chainId: 2710,
+      gas: 5500000,
+      accounts: { mnemonic: MNEMONIC }
     },
     polygontestnet: {
       url: POLYGON_TESTNET_PROVIDER,
       chainId: 80001,
       gas: 5500000,
-      accounts: { mnemonic: MNEMONIC },
-      from: ADDRESS_1
+      accounts: { mnemonic: MNEMONIC }
     },
+    hardhat: {},
     localhost: {
       url: "http://127.0.0.1:7545",
       accounts: { mnemonic: MNEMONIC },
       gasLimit: 6000000000,
       defaultBalanceEther: 10,
-    },
-    bsc: {
-      url: BSC_PROVIDER,
-      chainId: 56,
-      gas: 8000000,
-      accounts: { mnemonic: MNEMONIC },
-      from: ADDRESS_1
     }
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY
+    apiKey: POLYGON_API_KEY
   },
   solidity: {
-    version: "0.8.6",
+    version: "0.8.15",
     settings: {
       optimizer: {
         enabled: true,
